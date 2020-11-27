1df3adf898f1f333c89b4fa57e8d7a5de888eef0
hyong <hyong@hybing.onmicrosoft.com>
2020-11-27 16:44:11 +0800

    晚上最后的功能

--- wj-vue/src/components/library/SideMenu.vue
+++ wj-vue/src/components/library/SideMenu.vue
@@ -3,7 +3,8 @@
     class="categories"
     default-active="0"
     @select="handleSelect"
-    active-text-color="red">
+    active-text-color="red"
+  >
     <el-menu-item index="0">
       <i class="el-icon-menu"></i>
       <span slot="title">全部</span>
@@ -16,20 +17,20 @@
       <i class="el-icon-menu"></i>
       <span slot="title">流行</span>
     </el-menu-item>
-    <el-menu-item>
-      <i class="el-menu-item"></i>
+    <el-menu-item index="3">
+      <i class="el-icon-menu"></i>
       <span slot="title">文化</span>
     </el-menu-item>
-    <el-menu-item>
-      <i class="el-menu-item"></i>
+    <el-menu-item index="4">
+      <i class="el-icon-menu"></i>
       <span slot="title">生活</span>
     </el-menu-item>
-    <el-menu-item>
-      <i class="el-menu-item"></i>
+    <el-menu-item index="5">
+      <i class="el-icon-menu"></i>
       <span slot="title">经管</span>
     </el-menu-item>
-    <el-menu-item>
-      <i class="el-menu-item"></i>
+    <el-menu-item index="6">
+      <i class="el-icon-menu"></i>
       <span slot="title">科技</span>
     </el-menu-item>
   </el-menu>
@@ -37,16 +38,27 @@
 
 <script>
 export default {
-  name: 'SideMenu'
+  name: 'SideMenu',
+  data () {
+    return {
+      cid: ''
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      this.cid = key
+      this.$emit('indexSelect')
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .categories{
-    position: fixed;
-    margin-left: 50%;
-    left: -600px;
-    top: 100px;
-    width: 150px;
-  }
+.categories {
+  position: fixed;
+  margin-left: 50%;
+  left: -600px;
+  top: 100px;
+  width: 150px;
+}
 </style>

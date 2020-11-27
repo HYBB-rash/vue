9517bfd734df95b7ccd405a0fef78e5e1ee5cc14
hyong <hyong@hybing.onmicrosoft.com>
2020-11-27 13:34:26 +0800

    增添了book模块

--- wj-vue/src/components/library/LibraryIndex.vue
+++ wj-vue/src/components/library/LibraryIndex.vue
@@ -4,18 +4,25 @@
       <switch></switch>
       <SideMenu></SideMenu>
     </el-aside>
-    <el-main></el-main>
+    <el-main>
+      <books class="books-area"></books>
+    </el-main>
   </el-container>
 </template>
 
 <script>
 import SideMenu from './SideMenu'
+import Books from './Books'
 export default {
   name: 'AppLibrary',
-  components: {SideMenu}
+  components: {Books, SideMenu}
 }
 </script>
 
 <style scoped>
-
+  .books-area{
+    width: 990px;
+    margin-left: auto;
+    margin-right: auto;
+  }
 </style>
